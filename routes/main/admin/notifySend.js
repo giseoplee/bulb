d72a75a89d28832e59558f84b0a73b5d2039312a
giseoplee <llgs901@naver.com>
2015-12-03 16:49:32 +0900

    t
    t

--- routes/main/admin/notifySend.js
+++ routes/main/admin/notifySend.js
@@ -92,10 +92,6 @@ router.post('/', function(req, res, next) {
                   // sender.send(message, sendIds, 4, function (error, result){
                     
                   // });      
-                }
-              }
-            }
-        });  
       }else{
         res.status(200).json({"message" : "send_fail"});
       }
