a9a9738deb7d6d4bb257522ad03e0a4366c039ff
giseoplee <llgs901@naver.com>
2015-12-18 15:53:14 +0900

    listen
    listen port change

--- app.js
+++ app.js
@@ -88,4 +88,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-app.listen(3000);
+app.listen(8080);
--- routes/main/admin/test1.js
+++ routes/main/admin/test1.js
@@ -127,5 +127,4 @@ router.post('/', function(req, res, next) {
 });
 
 
-
 module.exports = router;
