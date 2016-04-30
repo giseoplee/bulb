f0e23966e59e84aac62b1eca11989ba2b975328c
giseoplee <llgs901@naver.com>
2016-04-30 12:10:14 +0900

    update
    update

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var test1 = require('./routes/main/admin/test1');
 var contents = require('./routes/temp/contents');
 var thumbnail = require('./routes/temp/thumbnails');
 var insertMember = require('./routes/Rest/MemberInsert.js');
+var versionCheck = require('./routes/VersionCheck.js');
+
 //var test2 = require('./routes/main/admin/test2');
 //var push_test = require('./routes/main/admin/push_test');
 
@@ -58,6 +60,7 @@ app.use('/main/admin/test1', test1);
 app.use('/contents', contents);
 app.use('/thumbnails', thumbnail);
 app.use('/member/new',insertMember);
+app.use('/version/check',versionCheck);
 //app.use('/main/admin/test2', test2);
 
 //app.use('/main/admin/push_test', push_test);
---
+++ routes/Rest/VersionCheck.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var mysql = require('mysql');
+var date = require('date-utils');
+var router = express.Router();
+
+var connection = mysql.createConnection({
+    'host':'bulbcommunocations.cj9khjm7u6ic.ap-northeast-1.rds.amazonaws.com',
+    'user':'admin',
+    'password':'Bcomms0116!',
+    'database':'services'
+});
+
+var date_instance = new Date();
+var insert_date = date_instance.toFormat("YYYY-MM-DD HH24:MI:SS");
+
+router.get('/', function(req, res, next) {
+
+  var query = connection.query("select id,name,version from applcations where application_id=?;",
+    [req.body.application_id],function(error, cursor){
+      if(error==null){
+        res.status(200).json({"version" : cursor[0].version, "message" : "success"});
+      }else{
+        console.log(query.sql);
+        res.status(200).json({"version" : 0, "message" : "query_fail"});
+      }
+  });
+});
+
+module.exports = router;
