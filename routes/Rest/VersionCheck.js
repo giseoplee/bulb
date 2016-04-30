f70861c6bd87b0e57b4b74cc97be8bebe6747aab
giseoplee <llgs901@naver.com>
2016-04-30 13:16:36 +0900

    .
    .

--- routes/Rest/VersionCheck.js
+++ routes/Rest/VersionCheck.js
@@ -12,17 +12,13 @@ var connection = mysql.createConnection({
     'database':'services'
 });
 
-// var date_instance = new Date();
-// var insert_date = date_instance.toFormat("YYYY-MM-DD HH24:MI:SS");
-
 router.get('/:application_id', function(req, res, next) {
 
   var query = connection.query("select id,name,version from applications where id=?;",
     [req.params.application_id],function(error, cursor){
-      if(error==null){
+      if(error==null && cursor.length > 0){
         res.status(200).json({"version" : cursor[0].version, "message" : "success"});
       }else{
-        console.log(query.sql);
         res.status(200).json({"version" : 0, "message" : "query_fail"});
       }
   });
